fa72111fb0f79cd6950eecf262b420f962e7eb7a
Vuong Hoang <hoangasphalt89@gmail.com>
2023-12-03 15:57:07 +0700

    check parenthesis

---
+++ W3_Parenthesis.cpp
@@ -0,0 +1,33 @@
+//C++ 
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check(string s) 
+{
+    stack<char> st;
+    for (char c : s) {
+        if (c == '(' || c == '[' || c == '{')
+            st.push(c);
+        else 
+        {
+            if (st.empty()) 
+                return false;
+            
+            char top = st.top();
+            st.pop();
+            if ((c == ')' && top != '(') || (c == ']' && top != '[') 
+                || (c == '}' && top != '{'))
+                return false;
+        }
+    }
+    return st.empty();
+}
+
+int main() 
+{
+    string s;
+    cin >> s;
+    cout << check(s) << endl;
+    return 0;
+}
+
