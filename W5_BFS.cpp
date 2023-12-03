930d609e8379c940d3de40a62b491f80e9f72260
Vuong Hoang <hoangasphalt89@gmail.com>
2023-12-03 16:03:07 +0700

    nodes visited by BFS

---
+++ W5_BFS.cpp
@@ -0,0 +1,55 @@
+//C++ 
+#include <bits/stdc++.h> 
+using namespace std;
+const int N = 100; // maximum number of nodes
+
+void bfs(int n, int m, bool graph[N][N]) 
+{
+  bool visited[N]; // array to mark the visited nodes
+  for (int i = 1; i <= n; i++) 
+    visited[i] = false; 
+
+  queue<int> q; // queue to store the nodes to be visited
+  for (int i = 1; i <= n; i++) {
+    if (!visited[i]) { // if the node is not visited
+      q.push(i); 
+      visited[i] = true; 
+      while (!q.empty()) { // while the queue is not empty
+        int u = q.front(); // get the front node of the queue
+        q.pop(); // pop it from the queue
+        cout << u << " "; // 
+        for (int v = 0; v < n; v++) { // for each adjacent node of u
+          if (graph[u][v] && !visited[v]) { // if it is not visited
+            q.push(v); 
+            visited[v] = true; 
+          }
+        }
+      }
+    }
+  }
+}
+
+void solve()
+{
+    int n, m; // number of nodes and edges
+    cin >> n >> m;
+    bool graph[N][N]; // adjacency matrix of the graph
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        graph[i][j] = false; // initialize the matrix as false
+      }
+    }
+    for (int i = 0; i < m; i++) {
+      int u, v; 
+      cin >> u >> v;
+      graph[u][v] = true; // mark the edge in the matrix
+      graph[v][u] = true; // mark the edge in the matrix (undirected graph)
+    }
+    bfs(n, m, graph);
+}
+
+int main() 
+{
+  solve(); 
+}
+
